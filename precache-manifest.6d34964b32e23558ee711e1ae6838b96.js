99b46881bf9676314a9943d1ec995fe671451821
azeez <ajimoh2012@yahoo.com>
2020-01-13 14:57:48 +0400

    Updates

--- precache-manifest.6d34964b32e23558ee711e1ae6838b96.js
+++ precache-manifest.6d34964b32e23558ee711e1ae6838b96.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24421206eb4482f26c404e8ba5dd8e86",
+    "revision": "68e6cb099506b7b7ee75c2f191a1dfe3",
     "url": "/robotfriends/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "5bad039498336ca9c5bd",
+    "revision": "a8f978368c7b136f98e0",
     "url": "/robotfriends/static/css/main.068a2c03.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robotfriends/static/js/2.d2f9d004.chunk.js.LICENSE"
   },
   {
-    "revision": "5bad039498336ca9c5bd",
-    "url": "/robotfriends/static/js/main.36689f9f.chunk.js"
+    "revision": "a8f978368c7b136f98e0",
+    "url": "/robotfriends/static/js/main.a3701238.chunk.js"
   },
   {
     "revision": "1dd135c7a88854b4c236",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robotfriends/precache-manifest.27416b70c1d1213e19b88bf8f2f7c477.js"
+  "/robotfriends/precache-manifest.6d34964b32e23558ee711e1ae6838b96.js"
 );
 
 self.addEventListener('message', (event) => {
