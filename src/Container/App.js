54dc3d54ded14cff39c7a0cf163b9f61d8c8a509
azeez <ajimoh2012@yahoo.com>
2020-01-13 15:30:51 +0400

    done

--- src/Container/App.js
+++ src/Container/App.js
@@ -28,17 +28,21 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                <CardList robots={filteredRobots}/>
-                </Scroll>
-            </div>
-        ); 
+        
+        if(this.state.robots.length === 0){
+            return  <h1>Loading</h1>
+        }else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                    <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            ); 
+        }
     }
-    
 }
 
 export default App;
